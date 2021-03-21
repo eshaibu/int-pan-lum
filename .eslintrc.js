703297cdb5b4161c5683d735a610e65f7058b693
Emmanuel Shaibu <emmanuel.shaibu@andela.com>
2021-03-21 15:44:08 +0100

    setup code linting

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "airbnb-typescript",
+    "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  ignorePatterns: ['public/**/*', 'build/**/*'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    "prettier/prettier": ["error", prettierOptions],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "react/jsx-filename-extension": ["warn", { "extensions": [".ts", ".tsx"] }],
+    "no-console": "warn",
+  },
+};
